a6f46af9d28eafb3c5b7e55792dcd34d5db4a823
ramusuco <yamamototakatowork@gmail.com>
2025-12-15 21:14:32 +0900

    fix : 作成場所調整

--- env_share/cmd/const.py
+++ env_share/cmd/const.py
@@ -2,8 +2,11 @@ import os
 
 ENVS = ["dev", "stg", "prd"]
 ENV_DIR = ".env"
+LATEST_DIR = os.path.join(ENV_DIR, "latest")
+KEYS_DIR = os.path.join(ENV_DIR, "keys")
 GITIGNORE_PATH = ".gitignore"
 ENCRYPTED_PREFIX = "encrypted:"
 ENC_DIR = "env_share"
-TMP_DIR = os.path.join("tmp", "env_share")
-GITIGNORE_REQUIRED = [f"{ENV_DIR}/*", TMP_DIR.replace(os.sep, '/') + "/*"]
+TMP_DIR ="tmp"
+WORK_DIR = os.path.join(TMP_DIR, "env_share")
+GITIGNORE_REQUIRED = [f"{ENV_DIR}/*", f"{TMP_DIR}/*", "*.keys"]
