1e7983d4ed83d2a1b2446078f6f7e1e471736b08
ramusuco <yamamototakatowork@gmail.com>
2025-12-15 21:15:20 +0900

    fix : UTF-8で固定

--- env_share/cmd/decryption.py
+++ env_share/cmd/decryption.py
@@ -6,21 +6,23 @@ from env_share.cmd.common import *
 
 logger = logging.getLogger(__name__)
 
-def main(target_env: str)-> None:
-    enc_file, _, env_file, work_enc_file, key_file = prepare_paths(target_env)
+def main(target_env: str) -> None:
+    enc_file, _, env_latest_file, work_enc_file, key_file = prepare_paths(target_env)
     ensure_gitignore()
     validate_files([key_file, enc_file])
 
     try:
         shutil.copy(enc_file, work_enc_file)
         run_decrypt(work_enc_file, key_file)
-        write_without_header(work_enc_file, env_file)
-        logger.info(f"wrote {env_file}")
+        write_without_header(work_enc_file, env_latest_file)
+        logger.info(f"wrote {env_latest_file}")
     finally:
         cleanup_tmp([work_enc_file])
+        ensure_encrypted_values(enc_file)
 
-def write_without_header(work_enc_file: str, env_file: str) -> None:
-    with open(work_enc_file, 'r') as src, open(env_file, 'w') as out:
+
+def write_without_header(work_enc_file: str, env_latest_file: str) -> None:
+    with open_file(work_enc_file, 'r') as src, open_file(env_latest_file, 'w') as out:
         started = False
         for line in src:
             stripped = line.strip()
--- env_share/cmd/encryption.py
+++ env_share/cmd/encryption.py
@@ -7,10 +7,11 @@ from env_share.cmd.common import *
 
 logger = logging.getLogger(__name__)
 
-def main(target_env: str)-> None:
+def main(target_env: str) -> None:
     enc_file, env_file, _, work_enc, key_file = prepare_paths(target_env)
     ensure_gitignore()
     created_new_enc = ensure_encrypted_file_exists(enc_file)
+
     if not created_new_enc:
         validate_files([key_file, enc_file, env_file])
     else:
@@ -34,25 +35,31 @@ def main(target_env: str)-> None:
         logger.info("Updated encrypted env file.")
     finally:
         cleanup_tmp([work_enc])
+        ensure_encrypted_values(enc_file)
 
-def embed_difference(env_file:str,work_enc_file:str)-> bool:
+
+def embed_difference(env_file: str, work_enc_file: str) -> bool:
     load_env_data = load_env_file(env_file)
-    load_enc_data = load_enc_file(work_enc_file)
-    add_data = {k:v for k,v in load_env_data.items() if k not in load_enc_data}
+    load_work_enc_data = load_enc_file(work_enc_file)
+    add_data = {k: v for k, v in load_env_data.items() if k not in load_work_enc_data}
     if not add_data:
+        logger.warning("New keys is not found.")
         return False
     add_data_to_plain_file(add_data, work_enc_file)
     return True
 
+
 def add_data_to_plain_file(add_data: dict, plain_file: str) -> None:
-    with open(plain_file, 'a') as f:
+    with open_file(plain_file, 'a') as f:
         for key, value in add_data.items():
             f.write(f"{key}={value}\n")
+            logger.info(f"【NEW KEY】: {key}")
+
 
 def ensure_encrypted_file_exists(enc_file: str) -> bool:
     if os.path.isfile(enc_file):
         return False
-    with open(enc_file, 'w'):
+    with open_file(enc_file, 'w'):
         pass
     logger.info(f"created empty encrypted env file: {enc_file}")
     return True
