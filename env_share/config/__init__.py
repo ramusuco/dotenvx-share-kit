f3e360bf88b29ee369c2fd4f81e618f0dc819c2c
ramusuco <yamamototakatowork@gmail.com>
2025-12-17 22:46:49 +0900

    chore

--- env_share/config/__init__.py
+++ env_share/config/__init__.py
@@ -1,12 +1,16 @@
 import os
 
+PROJECT_NAME = "env_share"
+
 ENVS = ["dev", "stg", "prd"]
 ENV_DIR = ".env"
 LATEST_DIR = os.path.join(ENV_DIR, "latest")
 KEYS_DIR = os.path.join(ENV_DIR, "keys")
 GITIGNORE_PATH = ".gitignore"
 ENCRYPTED_PREFIX = "encrypted:"
-ENC_DIR = "env_share"
+ENC_DIR = os.path.join(PROJECT_NAME, "encrypted")
 TMP_DIR = "tmp"
-WORK_DIR = os.path.join(TMP_DIR, "env_share")
-GITIGNORE_REQUIRED = [f"{ENV_DIR}/*", f"{TMP_DIR}/*", "*.keys"]
+WORK_DIR = os.path.join(TMP_DIR, PROJECT_NAME)
+
+IGNORE_WORK_DIR = f"{TMP_DIR}/{PROJECT_NAME}"
+GITIGNORE_REQUIRED = [f"{ENV_DIR}/*", f"{IGNORE_WORK_DIR}/*", "*.keys"]
